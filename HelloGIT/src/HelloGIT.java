5af8ec9f83152e6097e5419e368095841ea209ba
Hakchin Kim <hkim@pivotal.io>
2014-08-16 19:34:30 +0900

    test

---
+++ HelloGIT/src/HelloGIT.java
@@ -0,0 +1,22 @@
+/************************************************************************* 
+*  Compilation:  javac HelloGIT.java
+*  Execution:    java HelloGIT 
+* 
+*  Prints "Hello, GIT". By tradition, this is everyone's first program. 
+* 
+*  % java HelloGIT 
+*  Hello, GIT 
+* 
+*  These 17 lines of text are comments. They are not part of the program; 
+*  they serve to remind us about its properties. The first two lines tell 
+*  us what to type to compile and test the program. The next line describes 
+*  the purpose of the program. The next few lines give a sample execution 
+*  of the program and the resulting output. We will always include such 
+*  lines in our programs and encourage you to do the same. 
+* 
+*************************************************************************/
+public class HelloGIT {
+    public static void main(String[] args) {
+        System.out.println("hello GIT!");
+    }
+}
