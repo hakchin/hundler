27e3d8db519860a6cf8ae2b312d221d639b45f81
Hakchin Kim <hkim@pivotal.io>
2014-08-23 23:41:04 +0900

    modified:   helloruby.rb

--- helloruby/helloruby.rb
+++ helloruby/helloruby.rb
@@ -1,6 +1,10 @@
-x = 5;
+x = 4;
+
 # If x is equal to 5
 if x == 5
-# Print out this
-puts "x equals 5"
+  # Print out this
+  puts "x equals 5"
+else
+  # print x does not equal to 5
+  puts "x does not equal 5"
 end
