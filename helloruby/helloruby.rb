31c35f2be74265f092d61148379141f335ed60c5
Hakchin Kim <hkim@pivotal.io>
2014-08-24 23:45:42 +0900

    test

--- helloruby/helloruby.rb
+++ helloruby/helloruby.rb
@@ -1,16 +1,6 @@
-counter = 0
-
-while counter < 20
-  loop do
-    if counter > 5
-      break
-    elsif counter == 5
-      puts "do loop last"
-    end
-    puts counter
-    counter += 1
-  end
-  puts counter
-  puts "while loop"
-  counter += 2
+string_array = ["Ruby","In","A","Day"]
+# Make a counter
+string_array.each_with_index do |lv_itm,lv_idx|
+  # prints out the string
+  puts "Index #{lv_idx}: #{lv_itm}"
 end
