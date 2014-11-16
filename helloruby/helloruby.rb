e7db9d2cbe58018bea9ddf7e6260c1fcb2d30573
Hakchin Kim <sn2ro@me.com>
2014-11-16 09:53:51 +0900

    test
    test

--- helloruby/helloruby.rb
+++ helloruby/helloruby.rb
@@ -1,6 +1,17 @@
-string_array = ["Ruby","In","A","Day"]
-# Make a counter
-string_array.each_with_index do |lv_itm,lv_idx|
-  # prints out the string
-  puts "Index #{lv_idx}: #{lv_itm}"
-end
+# Make a new hash
+my_lazy_hash = Hash.new
+
+# Ask user for their firstname
+print "Please input your first name: "
+my_lazy_hash["firstname"] = gets.chomp.capitalize
+
+# Ask user for their lasttname
+print "Please input your last name: "
+my_lazy_hash["lastname"] = gets.chomp.capitalize
+
+# Ask user for their nickname
+print "Please input your nickname: "
+my_lazy_hash["nickname"] = gets.chomp.capitalize
+
+# Print out the hash
+puts my_lazy_hash
